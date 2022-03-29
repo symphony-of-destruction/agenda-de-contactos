d338974baabd0aed1d76fe79681543dd6d1d4561
Joseph Estevez <joseph.estevez@protonmail.com>
2022-03-29 19:22:28 -0400

    🗿

--- src/app/server.js
+++ src/app/server.js
@@ -5,17 +5,17 @@ import session from 'express-session';
 export default (config) => {
   const server = express();
 
+  server.use(express.json());
   server.use(express.urlencoded({ extended: false }));
-  server.set('view engine', 'ejs');
-  server.set('views', path.join(process.cwd(), 'src/views/pages'));
   server.use(
     session({
       secret: config.get('auth_key'),
       resave: false,
       saveUninitialized: true,
-      cookie: { identify: '', password: '' },
     }),
   );
+  server.set('view engine', 'ejs');
+  server.set('views', path.join(process.cwd(), 'src/views/pages'));
   server.use('/static', express.static(path.join(process.cwd(), 'public')));
 
   return server;
--- src/app/services/validateUser.service.js
+++ src/app/services/validateUser.service.js
@@ -3,16 +3,35 @@ import fs from 'fs';
 
 export default class ValidateUser {
   constructor() {
-    const data = JSON.parse(
+    this.users = this.loadData().users;
+  }
+
+  loadData() {
+    return JSON.parse(
       fs.readFileSync(join(process.cwd(), 'src/data/object.json')),
     );
-
-    this.users = data.users;
   }
 
   validate({ name, password }) {
     return this.users.find(
       (user) => user.name == name && user.password == password,
     );
+  }
+
+  saveData({ name, password, data }) {
+    const user = this.validate({ name, password });
+    if (user) {
+      const userId = this.users.findIndex((found) => found.name == user.name);
+      const save = this.loadData();
+      user.data = data;
+      this.users[userId] = user;
+      save.users = this.users;
+
+      fs.writeFileSync(
+        join(process.cwd(), 'src/data/object.json'),
+        JSON.stringify(save),
+      );
+    }
+    return true;
   }
 }
