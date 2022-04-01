1bda8d0bd8edc085d661289059978e9cb059e344
Joseph Estevez <joseph.estevez@protonmail.com>
2022-04-01 17:49:36 -0400

    endgame

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import WebApplication from './app';
 
 const app = WebApplication.build();
-const config = WebApplication.configService();
+// const config = WebApplication.configService();
 
-app.run(config.get('PORT'));
+app.run(process.env.PORT);
